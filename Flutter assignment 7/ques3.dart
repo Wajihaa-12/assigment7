a4cb4aa7a1c796b02cf27dc2d0c5dd99b4786917
Wajihaa-12 <146022225+Wajihaa-12@users.noreply.github.com>
2024-04-27 21:17:14 +0000

    initial commits

--- Flutter assignment 7/ques3.dart
+++
@@ -1,7 +0,0 @@
-//Q3. Write a program that prints the multiplication table of a given number using a for loop
-void main() {
-  int number = 5;
-  for (var i = 1; i <= 10; i++) {
-    print('${number} x ${i} = ${i * number}');
-  }
-}
--- Flutter assignment 7/ques6.dart
+++
@@ -1,9 +0,0 @@
-//Q6. Write a program that takes a list of numbers as input and prints the numbers greater than 5 using a for loop and  if-else condition.
-void main() {
-  List num = [2, 3, 0, 3, 6, 4, 18, 99, 204, 14, 16, 277];
-  for (var i = 0; i < num.length; i++) {
-    if (num[i] > 5) {
-      print(num[i]);
-    }
-  }
-}
